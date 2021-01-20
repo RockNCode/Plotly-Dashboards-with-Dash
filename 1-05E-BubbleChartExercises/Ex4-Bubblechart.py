d41bed23df9d34b245a6c95177de0ffc2570fef5
Manuel Garcia <alexgarcia2k@gmail.com>
2021-01-20 03:11:12 -0800

    Bubble chart ex

--- 1-05E-BubbleChartExercises/Ex4-Bubblechart.py
+++ 1-05E-BubbleChartExercises/Ex4-Bubblechart.py
@@ -6,14 +6,31 @@
 
 # Perform imports here:
 
-
+import plotly.offline as pyo
+import plotly.graph_objs as go
+import pandas as pd
 
 
 # create a DataFrame from the .csv file:
 
-
+df = pd.read_csv("../data/mpg.csv")
+print(df)
+data = [go.Scatter(          # start with a normal scatter plot
+    x=df['weight'],
+    y=df['horsepower'],
+    text=df['name'],
+    mode='markers',
+    marker=dict(size=df['weight']/100) # set the marker size
+)]
 # create data by choosing fields for x, y and marker size attributes
-
+layout = go.Layout(
+    title='horsepower vs. weight',
+    xaxis = dict(title = 'weight'), # x-axis label
+    yaxis = dict(title = 'horsepower'),        # y-axis label
+    hovermode='closest'
+)
+fig = go.Figure(data=data, layout=layout)
+pyo.plot(fig, filename='output.html')
 
 
 
