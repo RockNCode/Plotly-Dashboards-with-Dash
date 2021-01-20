f6725d20711b18dd7e4ff76c1560b87611a7fe18
Manuel Garcia <alexgarcia2k@gmail.com>
2021-01-20 05:47:51 -0800

    Box plot excercise.

--- 1-06E-BoxPlotExercises/Ex5-Boxplot.py
+++ 1-06E-BoxPlotExercises/Ex5-Boxplot.py
@@ -7,32 +7,40 @@
 
 # Perform imports here:
 
-
-
-
+import plotly.offline as pyo
+import plotly.graph_objs as go
+import pandas as pd
+import numpy as np
 
 # create a DataFrame from the .csv file:
+df = pd.read_csv('../data/abalone.csv')
 
-
+# print(df)
 # take two random samples of different sizes:
 
-
+a = np.random.choice(df['rings'],30,replace=False)
+b = np.random.choice(df['rings'],30,replace=False)
 
 # create a data variable with two Box plots:
+data = [
+    go.Box(
+        y=a,
+        name='A box'
 
-
-
-
-
-
-
-
-
-
+    ),
+    go.Box(
+        y=b,
+        name='B box'
+    )
+]
 
 # add a layout
 
-
-
+# add a layout
+layout = go.Layout(
+    title = 'Comparison of two samples taken from the same population'
+)
 
 # create a fig from data & layout, and plot the fig
+fig = go.Figure(data=data, layout=layout)
+pyo.plot(fig, filename='output.html')
